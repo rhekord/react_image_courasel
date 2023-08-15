808e43572f9a02794c1398f94895ef6cdcc60b63
rhekord <soanum001@st.ug.edu.gh>
2023-08-15 21:41:09 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+ import ImageSlider from './components/ImageSlider';
+import { SliderData } from './components/SliderData';
+
+function App() {
+  return (
+      <ImageSlider slides = { SliderData} />
+
+  )
+}
+
+export default App
---
+++ src/components/SliderData.js
@@ -0,0 +1,12 @@
+export const SliderData = 
+[
+    {
+        image:'https://images.unsplash.com/photo-1630710478039-9c680b99f800?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bmFydXRvfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+        image:'https://images.unsplash.com/photo-1621478374422-35206faeddfb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8b25lJTIwcGllY2UlMjBhbmltZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+        image:'https://images.unsplash.com/photo-1640753820600-bbe05979754c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fG9uZSUyMHBpZWNlJTIwYW5pbWV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+    }
+];
